316dc4606eb92ad5a404be9985bb29cfae0e5732
abdu <abdu>
2024-06-06 13:11:47 +0500

    JJss

---
+++ script.js
@@ -0,0 +1,15 @@
+var son = +prompt("Qo'ylar sonini kiriting");
+
+if (isNaN(son) || son <= 0) {
+  console.log("NaN");
+} else {
+  var qoylar = '';
+  for (var i = 1; i <= son; i++) {
+    if (i == 1) {
+      qoylar += i + " qo'y";
+    } else {
+      qoylar += ' ' + i + " qo'ylar";
+    }
+  }
+  console.log(qoylar);
+}
